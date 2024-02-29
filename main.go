8b24401d4d58f9276afc398cc71215fb3696ad7d
tejas122125 <tejasweekumarsingh@gmail.com>
2024-02-29 11:58:57 +0530

    tejas

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 func crawl (){
-	
+
 }
 func main(){
 	worklist := make(chan []string)
@@ -10,7 +10,23 @@ func main(){
 		worklist<- []string{basedomain}
 	}()
 	seen :=  make (map[string]bool)
-	list := worklist
+	for n:=1 ; n>0; n--{
+		list := worklist
+
+		for _,link := range <-worklist{
+			if !seen[link]{
+				seen[link] =true
+				n++
+				go func (link string, basedomain string)  {
+				foundLinks := crawl(link,basedomain)
+				if foundLinks != nil{
+					worklist<- foundLinks
+				}
+
+				}()
+			}
+		}
+	}
 
 
 }
