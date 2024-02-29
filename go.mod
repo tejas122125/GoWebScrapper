8b5e8ba10546075ba0a0b82fef4793909cf903b9
tejas122125 <tejasweekumarsingh@gmail.com>
2024-02-29 11:27:24 +0530

    tejas

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/tejas122125/GoWebScrapper
 
 go 1.21.5
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.0 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	golang.org/x/net v0.21.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+func crawl (){
+	
+}
+func main(){
+	worklist := make(chan []string)
+	basedomain := "website address"
+	go func ()  {
+		worklist<- []string{basedomain}
+	}()
+	seen :=  make (map[string]bool)
+	list := worklist
+
+
+}
