888faa630390721f054a3bd2d29f0549517c69af
tejas122125 <tejasweekumarsingh@gmail.com>
2024-02-29 13:18:36 +0530

    tejas

--- main.go
+++ main.go
@@ -1,19 +1,69 @@
 package main
 
-import "fmt"
-	list := worklist
-	func getRequest(targetUrl string)(error,[]string){
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
+	"github.com/PuerkitoBio/goquery"
+)
+
+
+
+var userAgents = []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"}
+
+
+
+func getRandomAgent(){
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Int() % len(userAgents)
+	return userAgents[randNum]
+}
+	func getRequest(targetUrl string)(*http.Response,error){
+
+client:=  &http.Client{}
+
+req,err := http.NewRequest("GET",targetUrl,nil)
+if err != nil {
+	return nil,err
+}else{
+	return res,nil
+}
+
+req.Header.Set("user-agent",getRandomAgent())
+res,err := client.Do(req)
+if err != nil {
+	return nil,err
+}else{
+	return res,nil
+}
 
 
 }
 
-func resolveRelativeLinks(link string, baseurl string){
+func resolveRelativeLinks(link string, baseurl string)(bool,string){
 
 }
 
 
-func discoverLinks(resp string,baseurl string)[]string{
+func discoverLinks(resp *http.Response,baseurl string)[]string{
+	if (resp !=nil){
+	doc , _ := goquery.NewDocumentFromResponse(resp)
+foundurls := []string{}
+	if doc != nil{
+
+	}
+	return foundurls
+	}else{
+return []string {}
+	}
 
 }
 
@@ -24,8 +74,14 @@ func crawl(targetUrl string, baseUrl string) {
 	links := discoverLinks(resp,baseUrl)
 	foundUrls := []string{}
 	for _,link := range links{
-		resolveRelativeLinks(link,baseUrl)
+		ok,correctLinks := resolveRelativeLinks(link,baseUrl)
+		if ok{
+			if correctLinks != ""{
+				foundUrls = append(foundUrls,correctLinks)
+			}
+		}
 	}
+// here we [parsse] the html content of our choice of links using go query
 
 
 
